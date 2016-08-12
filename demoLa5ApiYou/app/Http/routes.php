ffdb68647d3b3742c78d4557d253f83da610df28
Andres <cornejo.cruz.andres@gmail.com>
2016-08-11 22:46:51 -0500

    first sucessful test, show info about a video

--- demoLa5ApiYou/app/Http/routes.php
+++ demoLa5ApiYou/app/Http/routes.php
@@ -1,16 +1,11 @@
 <?php
 
-use Alaouy\Youtube\Youtube;
+use Youtube as y;
 
 
-Route::get('/', function () {
-    
-	$TEST_API_KEY = 'AIzaSyDDefsgXEZu57wYgABF7xEURClu4UAzyB8';
-    $youtube = new Youtube($TEST_API_KEY);
-
-    $vID = 'rie-hPVJ7Sw';
-    $video = $youtube->getVideoInfo($vID);
-
-	//$video = Youtube::getVideoInfo('rie-hPVJ7Sw');
-	return view( 'welcome', [ 'video' => $video ] );
+Route::get('/', function () 
+{
+    $video = Youtube::getVideoInfo('rie-hPVJ7Sw');
+	$video = $array = get_object_vars($video);
+	return $video;
 });
